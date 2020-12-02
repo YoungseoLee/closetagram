552e69655204ddcb191ed6a92cb51a3c680cf970
YoungSeo Lee <lee@Leeui-MacBookPro.local>
2020-12-03 06:17:29 +0900

    implementation favorte photo printed in heart icon page

--- app/src/main/java/com/example/closetagram/navigation/AlarmFragment.kt
+++ app/src/main/java/com/example/closetagram/navigation/AlarmFragment.kt
@@ -1,23 +1,31 @@
 package com.example.closetagram.navigation
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import androidx.appcompat.widget.LinearLayoutCompat
 import androidx.fragment.app.Fragment
-import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.bumptech.glide.request.RequestOptions
+import com.example.closetagram.MainActivity
 import com.example.closetagram.R
-import com.example.closetagram.navigation.model.AlarmDTO
-import com.google.firebase.auth.FirebaseAuth
+import com.example.closetagram.navigation.model.ContentDTO
 import com.google.firebase.firestore.FirebaseFirestore
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.fragment_alarm.*
 import kotlinx.android.synthetic.main.fragment_alarm.view.*
-import kotlinx.android.synthetic.main.item_comment.view.*
-import java.util.zip.Inflater
+import java.util.*
+import kotlin.collections.ArrayList
 
 class AlarmFragment : Fragment() {
+    var firestore: FirebaseFirestore? = null
+    var fragmentView: View? = null
+    var fragmentViewAdapter: UserFragmentRecycleriewAdapter? = null
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -25,73 +33,57 @@ class AlarmFragment : Fragment() {
         savedInstanceState: Bundle?
 
     ): View? {
-        var view = LayoutInflater.from(activity).inflate(R.layout.fragment_alarm, container, false)
-        view.alarmfragment_recyclerview.adapter = AlarmRecyclerviewAdapter()
-        view.alarmfragment_recyclerview.layoutManager = LinearLayoutManager(activity)
-
-        return view
+        var mainactivity = (activity as MainActivity)
+        var contentList = ArrayList<ContentDTO>();
+        fragmentView =
+            LayoutInflater.from(activity).inflate(R.layout.fragment_alarm, container, false)
+        firestore = FirebaseFirestore.getInstance()
+        fragmentViewAdapter = UserFragmentRecycleriewAdapter()
+        fragmentView?.alarmfragment_recyclerview?.adapter = fragmentViewAdapter
+        fragmentView?.alarmfragment_recyclerview?.layoutManager = GridLayoutManager(activity, 3)
+        return fragmentView
     }
 
-    inner class AlarmRecyclerviewAdapter : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
-        var alarmDTOList: ArrayList<AlarmDTO> = arrayListOf()
+    inner class UserFragmentRecycleriewAdapter() :
+        RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+        var contentFilterList: ArrayList<ContentDTO> = arrayListOf()
 
         init {
-            var uid = FirebaseAuth.getInstance().currentUser?.uid
-            FirebaseFirestore.getInstance().collection("alarms")
-                .whereEqualTo("destinationUid", uid)
-                .addSnapshotListener { querySnapshot, firebaseFirestoreException ->
-                    alarmDTOList.clear()
+            firestore?.collection("images")
+                ?.addSnapshotListener { querySnapshot, firebaseFirestoreException ->
+                    //Sometime, This code return null of querySnapshot when it signout
                     if (querySnapshot == null) return@addSnapshotListener
 
+                    //Get data
                     for (snapshot in querySnapshot.documents) {
-                        alarmDTOList.add(snapshot.toObject(AlarmDTO::class.java)!!)
+                        if (snapshot.toObject(ContentDTO::class.java)!!.favoriteCount > 0) {
+                            contentFilterList.add(snapshot.toObject(ContentDTO::class.java)!!)
+                        }
                     }
                     notifyDataSetChanged()
                 }
         }
 
         override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
-            var view =
-                LayoutInflater.from(parent.context).inflate(R.layout.item_comment, parent, false)
-
-            return CustomViewHolder(view)
+            var width = resources.displayMetrics.widthPixels / 3
+            var imageview = ImageView(parent.context)
+            imageview.layoutParams = LinearLayoutCompat.LayoutParams(width, width)
+            return CustomviewHolder(imageview)
         }
 
-        inner class CustomViewHolder(view: View) : RecyclerView.ViewHolder(view)
+        inner class CustomviewHolder(var imageview: ImageView) :
+            RecyclerView.ViewHolder(imageview) {
+
+        }
 
         override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
-            var view = holder.itemView
-
-            FirebaseFirestore.getInstance().collection("profileImages")
-                .document(alarmDTOList[position].uid!!).get().addOnCompleteListener { task ->
-                    if (task.isSuccessful) {
-                        var url = task.result!!["image"]
-                        Glide.with(view.context).load(url).apply(RequestOptions().circleCrop())
-                            .into(view.commentviewitem_imageview_profile)
-                    }
-                }
-            when (alarmDTOList[position].kind) {
-                0 -> {
-                    val str_0 =
-                        alarmDTOList[position].userId + getString(R.string.alarm_favorite)
-                    view.commentviewitem_textview_profile.text = str_0
-                }
-                1 -> {
-                    val str_0 =
-                        alarmDTOList[position].userId + " " + getString(R.string.alarm_comment) + " of " + alarmDTOList[position].message
-                    view.commentviewitem_textview_profile.text = str_0
-                }
-                2 -> {
-                    val str_0 =
-                        alarmDTOList[position].userId + " " + getString(R.string.alarm_follow)
-                    view.commentviewitem_textview_profile.text = str_0
-                }
-            }
-            view.commentviewitem_textview_comment.visibility = View.INVISIBLE
+            var imageview = (holder as CustomviewHolder).imageview
+            Glide.with(holder.itemView.context).load(contentFilterList[position].imageUrl)
+                .apply(RequestOptions().centerCrop()).into(imageview)
         }
 
         override fun getItemCount(): Int {
-            return alarmDTOList.size
+            return contentFilterList.size
         }
     }
 }
